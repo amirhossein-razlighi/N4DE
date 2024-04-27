67c8398ad2a59190caea18efd16efd45b2bc0ecd
amirhossein-razlighi <arazlighi@gmail.com>
2024-04-27 13:42:01 +0000

    add SLURM compatibility

---
+++ job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=NIE
+#SBATCH --partition=debug       # Specify the partition
+#SBATCH --nodes=1               # Request 1 node
+#SBATCH --ntasks=1              # Number of tasks (total)
+#SBATCH --cpus-per-task=8       # Number of CPU cores (threads) per task
+#SBATCH --mem-per-cpu=4G        # Memory limit per CPU core (there is no --mem-per-task)
+#SBATCH --time=02:15:00         # Job timeout
+#SBATCH --output=./job_out/myjob.log      # Redirect stdout to a log file
+#SBATCH --error=./job_out/myjob.error     # Redirect stderr to a separate error log file
+#SBATCH --mail-type=ALL         # Send updates via email
+#SBATCH --gpus=l4-24g:2      # Request 2 GPUs of model L4
+
+srun -- mkenv -f /home/amirhossein_razlighi/codes/NIE_Animated/test-env.yml -- 
+cd nvdiffrast/ 
+pip install .
+cd ..
+python3 main_animated.py --config ./config/animation_deform.yaml
