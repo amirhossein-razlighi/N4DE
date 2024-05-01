2e13a0f95dc76699dcc4facd7f34d022d41498f6
amirhossein-razlighi <arazlighi@gmail.com>
2024-05-01 14:20:02 +0000

    add jobs

--- render_test_job.sh
+++ render_test_job.sh
@@ -5,18 +5,21 @@
 #SBATCH --ntasks=1              # Number of tasks (total)
 #SBATCH --cpus-per-task=4       # Number of CPU cores (threads) per task
 #SBATCH --mem-per-cpu=4G        # Memory limit per CPU core (there is no --mem-per-task)
-#SBATCH --time=00:05:00         # Job timeout
-#SBATCH --output=./job_out/myjob.log      # Redirect stdout to a log file
-#SBATCH --error=./job_out/myjob.error     # Redirect stderr to a separate error log file
+#SBATCH --time=00:15:00         # Job timeout
+#SBATCH --output=./job_out/render_out.log      # Redirect stdout to a log file
+#SBATCH --error=./job_out/render_err.error     # Redirect stderr to a separate error log file
 #SBATCH --mail-type=ALL         # Send updates via email
 #SBATCH --gpus=l4-24g:1      # Request 1 GPU of model L4
 
 srun -- mkenv -f /home/amirhossein_razlighi/codes/NIE_Animated/test-env.yml -- \
-bash -c "cd nvdiffrast/ && \
-export CC=/usr/bin/gcc-12 && \
-export CXX=/usr/bin/g++-12 && \
-export CUDA_HOME="/opt/modules/nvidia-cuda-12.3/" && \
+bash -c \
+"export IMAGEIO_FFMPEG_EXE="/usr/bin/ffmpeg" && \
+cd nvdiffrast/ && \
 pip install . && \
 cd .. && \
-export IMAGEIO_FFMPEG_EXE="/usr/bin/ffmpeg" && \
-python3 render.py"
+python3 render_a_mesh.py"
+
+# python3 render.py
+# export CC=/usr/bin/gcc-12 && \
+# export CXX=/usr/bin/g++-12 && \
+# export CUDA_HOME="/opt/modules/nvidia-cuda-12.3/" && \
