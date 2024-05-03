c4787da5a372ed9a9b6a23563a50868e1c43a493
amirhossein-razlighi <arazlighi@gmail.com>
2024-05-03 14:08:58 +0000

    rename training with single an multi gpus

--- train_single.sh
+++ train_single.sh
@@ -5,20 +5,18 @@
 #SBATCH --ntasks=1              # Number of tasks (total)
 #SBATCH --cpus-per-task=8       # Number of CPU cores (threads) per task
 #SBATCH --mem-per-cpu=4G        # Memory limit per CPU core (there is no --mem-per-task)
-#SBATCH --time=08:30:00         # Job timeout
+#SBATCH --time=12:00:00         # Job timeout
 #SBATCH --output=./job_out/train_out.log      # Redirect stdout to a log file
 #SBATCH --error=./job_out/train_err.error     # Redirect stderr to a separate error log file
 #SBATCH --mail-type=ALL         # Send updates via email
-#SBATCH --gpus=l4-24g:2         # Request 2 GPUs of model L4
+#SBATCH --gpus=l4-24g:1         # Request 1 GPU of model L4
 
 srun -- mkenv -f /home/amirhossein_razlighi/codes/NIE_Animated/test-env.yml -- \
-/usr/bin/sh -c "
-export IMAGEIO_FFMPEG_EXE="/usr/bin/ffmpeg" && \
-pip uninstall nvdiffrast -y && \
-cd nvdiffrast/ && \
+/usr/bin/sh -c "cd nvdiffrast/ && \
 pip install . && \
 cd .. && \
-torchrun --standalone --nproc_per_node=2 main_distributed.py --config ./config/animation_deform.yaml"
+export IMAGEIO_FFMPEG_EXE="/usr/bin/ffmpeg" && \
+python3 main_animated.py --config ./config/animation_deform.yaml"
 
 # export CC=/usr/bin/gcc-12 && \
 # export CXX=/usr/bin/g++-12 && \
